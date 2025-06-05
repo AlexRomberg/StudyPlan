1e78f705ba630126272fd5be4f0d173e2cf22295
Alex <42290929+AlexRomberg@users.noreply.github.com>
2025-06-05 17:06:51 +0200

    small cleanup

--- src/app/util/icons.ts
+++ src/app/util/icons.ts
@@ -1,4 +1,23 @@
-import { icons as ico } from "lucide-angular";
+import { Award, Check, ChevronDown, ChevronUp, CircleCheck, ExternalLink, FileText, Grid3x3, Heart, Info, Languages, LayoutList, MessageCircleWarning, Star, SunSnow, UserRound, X, Lock } from "lucide-angular";
 
 
-export const icons = ico
+export const icons = {
+    Award,
+    Check,
+    ChevronDown,
+    ChevronUp,
+    CircleCheck,
+    ExternalLink,
+    FileText,
+    Grid3x3,
+    Heart,
+    Info,
+    Languages,
+    LayoutList,
+    Lock,
+    MessageCircleWarning,
+    Star,
+    SunSnow,
+    UserRound,
+    X
+}
